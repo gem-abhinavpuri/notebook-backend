3e235cecca6a462efe7a4a40c03a3bb9b3abe25d
Abhinav <66725911+Abhinav16072001@users.noreply.github.com>
2024-03-02 20:23:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const connectToMongo = require('./db');
 const express = require('express');
-var cors = require('cors');
+const cors = require('cors');
 
 connectToMongo();
 const app = express();
@@ -13,7 +13,11 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
+// Send welcome message
+app.get('/', (req, res) => {
+    res.send('Welcome to Notebook backend!');
+});
 
 app.listen(port, () => {
     console.log(`Notebook backend listening at http://localhost:${port}`);
-})
+});
