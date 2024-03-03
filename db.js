f8867b20879ab1f664ff857563a552a305881e90
gem-abhinavpuri <122994296+gem-abhinavpuri@users.noreply.github.com>
2024-03-03 13:23:10 +0530

    a

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://Abhinav:gbGy07EeiwiNvhTe@cluster0.urvmp8c.mongodb.net/";
+const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
-const connectToMongo = () => {
-    mongoose.connect(mongoURI, () => {
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI, ()=>{
         console.log("Connected to Mongo Successfully");
     })
 }
--- routes/notes.js
+++ routes/notes.js
@@ -1,27 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require("body-parser");
-const Note = require('../models/Note');
 const fetchuser = require('../middleware/fetchuser');
+const Note = require('../models/Note');
 const { body, validationResult } = require('express-validator');
-
-router.use(bodyParser.urlencoded({ extended: false }));
-router.use(express.json());
 
 // ROUTE 1: Get All the Notes using: GET "/api/notes/getuser". Login required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
     try {
         const notes = await Note.find({ user: req.user.id });
-        res.json(notes);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Internal Server Error");
-    }
-})
-
-router.get('/test',async (req, res) => {
-    try {
-        res.json({ "Success": "Working"});
+        res.json(notes)
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
@@ -34,6 +21,7 @@ router.post('/addnote', fetchuser, [
     body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
         try {
             const { title, description, tag } = req.body;
+
             // If there are errors, return Bad request and the errors
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
@@ -42,8 +30,10 @@ router.post('/addnote', fetchuser, [
             const note = new Note({
                 title, description, tag, user: req.user.id
             })
-            const savedNote = await note.save();
-            res.json(savedNote);
+            const savedNote = await note.save()
+
+            res.json(savedNote)
+
         } catch (error) {
             console.error(error.message);
             res.status(500).send("Internal Server Error");
@@ -81,11 +71,12 @@ router.delete('/deletenote/:id', fetchuser, async (req, res) => {
         // Find the note to be delete and delete it
         let note = await Note.findById(req.params.id);
         if (!note) { return res.status(404).send("Not Found") }
-        
+
         // Allow deletion only if user owns this Note
         if (note.user.toString() !== req.user.id) {
             return res.status(401).send("Not Allowed");
         }
+
         note = await Note.findByIdAndDelete(req.params.id)
         res.json({ "Success": "Note has been deleted", note: note });
     } catch (error) {
@@ -93,5 +84,4 @@ router.delete('/deletenote/:id', fetchuser, async (req, res) => {
         res.status(500).send("Internal Server Error");
     }
 })
-
-module.exports = router;
+module.exports = router
